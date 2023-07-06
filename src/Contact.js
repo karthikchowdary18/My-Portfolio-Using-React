0f3d6037b0a400ab8147ae408ab57cd2f0ac8c70
Nunna Karthik Chowdary <121443020+karthikchowdary18@users.noreply.github.com>
2023-07-07 01:29:54 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -1,35 +1,36 @@
-import React  from 'react';
+import React from 'react';
 import "./App.css";
 import github from "./github.jpg";
 import linkedin from"./ll.jpg";
 import mail from "./mail.jpg";
 
-const Contact= ()=> {
-    return (   
-      <div class ="hh">      
-     <h1 class="color-animation">contact me</h1>
-  <p class="color-animation">For contact and knowing my skills and previous works you can go through the below sites</p>
-   <div className="social-media">   
-  <div className="icon-container">
-    <img src={github} alt="github" className="icon" />
-    <a href="https://github.com/karthikchowdary18">
-      <button className="button">GitHub</button>
-    </a>
-  </div>
-  <div className="icon-container">
-    <img src={linkedin} alt="linkedin" className="icon" />
-    <a href="https://www.linkedin.com/in/karthik-chowdary-9248b9257/">
-      <button className="button">linkedin</button>
-    </a>
-  </div>
-  <div className="icon-container">
-    <img src={mail} alt="email" className="icon" />
-    <a href="mailto:karthikchowdary018@gmail.com">
-      <button className="button">Mail</button>
-    </a>
-  </div>
-</div>
-</div>  
-    )
-  }
+const Contact = () => {
+  return (
+    <div className="hh">
+      <h1 className="color-animation">Contact me</h1>
+      <p className="color-animation">For contact and knowing my skills and previous works, you can go through the below sites</p>
+      <div className="social-media">
+        <div className="icon-container">
+          <a href="https://github.com/karthikchowdary18" target="_blank" rel="noopener noreferrer">
+            <img src={github} alt="github" className="icon" />
+            <button className="button">GitHub</button>
+          </a>
+        </div>
+        <div className="icon-container">
+          <a href="https://www.linkedin.com/in/karthik-chowdary-9248b9257/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedin} alt="linkedin" className="icon" />
+            <button className="button">LinkedIn</button>
+          </a>
+        </div>
+        <div className="icon-container">
+          <a href="mailto:karthikchowdary018@gmail.com">
+            <img src={mail} alt="email" className="icon" />
+            <button className="button">Mail</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Contact;
